de41c08a0e3e596818176fd2a0b6452c906795b6
M-Soriano <104050536+M-Soriano@users.noreply.github.com>
2025-02-16 22:58:11 -0800

    Created the basemap tile layer.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,7 @@
 // Create the 'basemap' tile layer that will be the background of our map.
-
-
+let basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
 // OPTIONAL: Step 2
 // Create the 'street' tile layer as a second background of the map
 
