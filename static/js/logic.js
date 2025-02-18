89e584937035c2d9486b5854a2bfea4166965452
M-Soriano <104050536+M-Soriano@users.noreply.github.com>
2025-02-17 22:11:59 -0800

    In logic.js, inside static folder, added earthquake and tectonic_platess tile layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -20,7 +20,8 @@ basemap.addTo(map);
 // OPTIONAL: Step 2
 // Create the layer groups, base maps, and overlays for our two sets of data, earthquakes and tectonic_plates.
 // Add a control to the map that will allow the user to change which layers are visible.
-
+let earthquake = new L.LayerGroup();
+let tectonic_plates = new L.LayerGroup();
 
 // Make a request that retrieves the earthquake geoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
