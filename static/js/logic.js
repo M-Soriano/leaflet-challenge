44f56f85ec4e1fba82daa6638baaa84c7348cb62
M-Soriano <104050536+M-Soriano@users.noreply.github.com>
2025-02-18 04:30:25 -0800

    In logic.js, inside static folder, added popup for each marker

--- static/js/logic.js
+++ static/js/logic.js
@@ -84,6 +84,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     style: styleInfo,
     // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
     onEachFeature: function (feature, layer) {
+      let mg = feature.properties.mag
+      let lt = feature.properties.place
+      layer.bindPopup(`Magnitude: ${mg}<br>Location: ${lt}`)
+      
 
     }
   // OPTIONAL: Step 2
